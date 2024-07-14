536b9550704c9db1f5ba587cc0f504ca234e222f
Carla <carlasantos45@hotmail.com.br>
2024-07-14 17:24:20 -0300

    Alterar temas claro e escuro

---
+++ index.js
@@ -0,0 +1,13 @@
+function verificarTema(){
+    const temaArmazenado = localStorage.getItem('tema');
+    if(temaArmazenado){
+        document.body.setAttribute('data-tema', temaArmazenado);
+    }
+}
+
+function alterarTema(){
+    const tema = document.body.getAttribute('data-tema');
+    const novoTema = tema == 'dark' ? 'main' : 'dark'
+    document.body.setAttribute("data-tema", novoTema);
+    localStorage.setItem('tema', novoTema);
+}
